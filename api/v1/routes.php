03c469a2a47f700ac0ebdf641f1d1a500b7c5d45
Vasilii Polshakov <45201a@gmail.com>
2021-07-08 17:51:38 +0300

    some fix routes

--- api/v1/routes.php
+++ api/v1/routes.php
@@ -6,7 +6,7 @@ use Slim\Routing\RouteCollectorProxy;
 $group->group(
     '/test',
     function (RouteCollectorProxy $group) {
-        $group->get('/', function (Request $request, Response $response, array $args) {
+        $group->get('/', function (Psr\Http\Message\ServerRequestInterface $request,Psr\Http\Message\ResponseInterface  $response, array $args) {
 
             $response->getBody()->write("Test Hello");
             return $response;
